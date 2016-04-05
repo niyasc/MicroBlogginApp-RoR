dcd9412b3ca20e630979155650eeb47167a8611a
Niyas C <niyasmonc@gmail.com>
2016-04-05 09:51:49 +0000

    Finished authentication task, atleast login, logout actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login_user @user
       flash[:success] = "Welcome to MicroBlog RoR"
       redirect_to @user
     else
